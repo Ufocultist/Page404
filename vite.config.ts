935afb7045754d6a2a2a9901502ee10ec1afd092
Dmitrii Sheviakov <Dmitrii.Sheviakov@omnicell.com>
2026-02-19 16:24:46 -0500

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import autoprefixer from 'autoprefixer';
 import eslint from 'vite-plugin-eslint2';
 
 export default defineConfig({
-    base: '/Page404/',
+    base: './',
     plugins: [
         eslint()
     ],
